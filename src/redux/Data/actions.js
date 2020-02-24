05ca23ee4a3ab7f8025a4b5984fe7aecd5cc6e76
aziz gharbi <med.aziz,gharbii@gmail.com>
2020-02-24 02:17:06 -0500

    update action

--- src/redux/Data/actions.js
+++ src/redux/Data/actions.js
@@ -1,15 +1,13 @@
 import { GET_DATA } from "./constants";
 
-export const getData = () => {
-  return dispatch => {
-    return dispatch({
-      type: GET_DATA,
-      payload: [
-        {
-          name: "foo"
-        },
-        { name: "bar" }
-      ]
-    });
-  };
+export const getData = () => dispatch => {
+  return dispatch({
+    type: GET_DATA,
+    payload: [
+      {
+        name: "foo"
+      },
+      { name: "bar" }
+    ]
+  });
 };
