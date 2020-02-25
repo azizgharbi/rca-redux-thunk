6e2dfd11b83f90aab148b2ec27a5c5419a8999db
aziz gharbi <med.aziz,gharbii@gmail.com>
2020-02-25 16:35:34 -0500

    updates

--- src/App/style.js
+++ src/App/style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const AppWrapper = styled.div`
   @import url("https://fonts.googleapis.com/css?family=Raleway&display=swap");
   background: #d1531f;
+  border-radius: 12px;
   font-family: "Raleway", sans-serif;
   width: 70%;
   margin: auto;
